23712a2e2c99822d6b3908bf63c1b2dd356734b3
Rainer Simon <rainer@rainersimon.io>
2024-08-18 19:05:00 +0200

    Site structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,31 +2,52 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
     title: 'Annotorious',
     customCss: [
       './src/index.css',
     ],
+    expressiveCode: {
+      styleOverrides: { borderRadius: '0.25rem' },
+      themes: ['nord']
+    },
     social: {
       github: 'https://github.com/withastro/starlight'
     },
     sidebar: [{
       label: 'Start Here',
-      items: [
-      {
+      items: [{
         label: 'Getting Started',
         link: 'getting-started',
       }, {
+        label: 'OpenSeadragon',
+        link: 'openseadragon'
+      }, {
         label: 'Installation',
         slug: 'installation'
+      }, {
+        label: 'Migrating from v2.7',
+        link: 'migration-guide',
       }]
     }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
+      label: 'Guides',
+      items: [{
+        label: 'Annotating Images',
+        link: 'js/images'
+      }, {
+        label: 'OpenSeadragon',
+        link: 'js/openseadragon'
+      }]
+    }, {
+      label: 'Tutorials',
+      items: []
+    }, {
+      label: 'React',
+      autogenerate: { directory: 'react' }
+    }, {
+      label: 'Svelte',
+      autogenerate: { directory: 'svelte' }
     }]
   }), react()]
 });
