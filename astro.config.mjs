8b5efafe0603241ba24aa6f05435f0b6ed41fcc5
Rainer Simon <rainer@rainersimon.io>
2024-08-18 14:12:23 +0200

    Basic custom theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,16 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
-    title: 'My Docs',
+    title: 'Annotorious',
+    customCss: [
+      './src/index.css',
+    ],
     social: {
       github: 'https://github.com/withastro/starlight'
     },
     sidebar: [{
       label: 'Start Here',
       items: [
-      // Each item here is one entry in the navigation menu.
       {
         label: 'Getting Started',
         link: 'getting-started',
