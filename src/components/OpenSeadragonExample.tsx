12a49b3297a9f94cf666e61c3d9238d65954809b
Rainer Simon <rainer@rainersimon.io>
2024-08-20 14:45:15 +0200

    OpenSeadragon example

--- src/components/OpenSeadragonExample.tsx
+++ src/components/OpenSeadragonExample.tsx
@@ -1,7 +1,34 @@
-import { OpenSeadragonViewer } from '@annotorious/react';
+import { useEffect, useState } from 'react';
+import { Annotorious, OpenSeadragonAnnotator, OpenSeadragonViewer, ShapeType, useAnnotator } from '@annotorious/react';
+import type { AnnotoriousOpenSeadragonAnnotator } from '@annotorious/react';
 import OpenSeadragon from 'openseadragon';
+import { Move, Square, TriangleRight } from 'lucide-react';
 
 import './OpenSeadragonExample.css';
+import '@annotorious/react/annotorious-react.css';
+
+const ANNOTATION = {
+  id: '4c4ee2bf-50e0-4bad-8176-76372b6ccf4b',
+  bodies: [],
+  target: {
+    annotation: '4c4ee2bf-50e0-4bad-8176-76372b6ccf4b',
+    selector: {
+      type: ShapeType.RECTANGLE,
+      geometry: {
+        bounds: {
+            minX: 4713,
+            minY: 1859,
+            maxX: 4845,
+            maxY: 1956
+        },
+        x: 4713,
+        y: 1859,
+        w: 132,
+        h: 97
+      }
+    }
+  }
+}
 
 const options: OpenSeadragon.Options = {
   prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@latest/build/openseadragon/images/',
@@ -18,12 +45,62 @@ const options: OpenSeadragon.Options = {
   preserveImageSizeOnResize: true
 };
 
+export const App = () => {
+
+  const [tool, setTool] = useState<'rectangle' | 'polygon' | undefined>();
+
+  const anno = useAnnotator<AnnotoriousOpenSeadragonAnnotator>();
+
+  useEffect(() => {
+    if (!anno) return;
+
+    anno.addAnnotation(ANNOTATION);
+
+    return () => {
+      anno.clearAnnotations();
+    }
+  }, [anno]);
+
+  return (
+    <div className="openseadragon-example">
+      <OpenSeadragonAnnotator
+        drawingEnabled={tool !== undefined}
+        tool={tool || 'rectangle'}>
+        <OpenSeadragonViewer
+          className="osd-container"
+          options={options} />
+      </OpenSeadragonAnnotator>
+
+      <div className='actions'>
+        <button 
+          className={tool === undefined ? 'active' : undefined}
+          onClick={() => setTool(undefined)}>
+          <Move size={15} /> Move Image
+        </button>
+
+        <button
+          className={tool === 'rectangle' ? 'rectangle active' : 'rectangle'}
+          onClick={() => setTool('rectangle')}>
+          <Square size={15} /> Rectangle
+        </button>
+
+        <button
+          className={tool === 'polygon' ? 'polygon active' : 'polygon'}
+          onClick={() => setTool('polygon')}>
+          <TriangleRight size={15} /> Polygon
+        </button>
+      </div>
+    </div>
+  )
+
+}
+
 export const OpenSeadragonExample = () => {
 
   return (
-    <OpenSeadragonViewer
-      className="osd-container"
-      options={options} />
+    <Annotorious>
+      <App />
+    </Annotorious>
   )
 
 }
