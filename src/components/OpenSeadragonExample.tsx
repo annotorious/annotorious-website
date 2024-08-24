ab5bc8eb2755576c8f817fcb587ccb6c88b77e4e
Rainer Simon <rainer@rainersimon.io>
2024-08-24 17:13:37 +0200

    Refactoring, getting started page

--- src/components/OpenSeadragonExample.tsx
+++ src/components/OpenSeadragonExample.tsx
@@ -1,106 +1,118 @@
 import { useEffect, useState } from 'react';
-import { Annotorious, OpenSeadragonAnnotator, OpenSeadragonViewer, ShapeType, useAnnotator } from '@annotorious/react';
 import type { AnnotoriousOpenSeadragonAnnotator } from '@annotorious/react';
-import OpenSeadragon from 'openseadragon';
 import { Move, Square, TriangleRight } from 'lucide-react';
 
 import './OpenSeadragonExample.css';
 import '@annotorious/react/annotorious-react.css';
 
-const ANNOTATION = {
-  id: '4c4ee2bf-50e0-4bad-8176-76372b6ccf4b',
-  bodies: [],
-  target: {
-    annotation: '4c4ee2bf-50e0-4bad-8176-76372b6ccf4b',
-    selector: {
-      type: ShapeType.RECTANGLE,
-      geometry: {
-        bounds: {
-            minX: 4713,
-            minY: 1859,
-            maxX: 4845,
-            maxY: 1956
-        },
-        x: 4713,
-        y: 1859,
-        w: 132,
-        h: 97
+let OpenSeadragonExample: () => JSX.Element = () => null as unknown as JSX.Element; 
+
+if (typeof window !== 'undefined') {
+  const { 
+    Annotorious, 
+    OpenSeadragonAnnotator, 
+    OpenSeadragonViewer, 
+    ShapeType, 
+    useAnnotator 
+  } = await import('@annotorious/react');
+
+  const ANNOTATION = {
+    id: '4c4ee2bf-50e0-4bad-8176-76372b6ccf4b',
+    bodies: [],
+    target: {
+      annotation: '4c4ee2bf-50e0-4bad-8176-76372b6ccf4b',
+      selector: {
+        type: ShapeType.RECTANGLE,
+        geometry: {
+          bounds: {
+              minX: 4713,
+              minY: 1859,
+              maxX: 4845,
+              maxY: 1956
+          },
+          x: 4713,
+          y: 1859,
+          w: 132,
+          h: 97
+        }
       }
     }
   }
-}
 
-const options: OpenSeadragon.Options = {
-  prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@latest/build/openseadragon/images/',
-  tileSources: {
-    type: 'image',
-    url:  '/images/schoenbrunn.jpg'
-  },
-  gestureSettingsMouse: {
-    clickToZoom: false,
-    dblClickToZoom: false
-  },
-  minZoomLevel: 0.8,
-  visibilityRatio: 0.2,
-  preserveImageSizeOnResize: true
-};
+  const options: OpenSeadragon.Options = {
+    prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@latest/build/openseadragon/images/',
+    tileSources: {
+      type: 'image',
+      url:  '/images/schoenbrunn.jpg'
+    },
+    gestureSettingsMouse: {
+      clickToZoom: false,
+      dblClickToZoom: false
+    },
+    minZoomLevel: 0.8,
+    visibilityRatio: 0.2,
+    preserveImageSizeOnResize: true
+  };
 
-export const App = () => {
+  const App = () => {
 
-  const [tool, setTool] = useState<'rectangle' | 'polygon' | undefined>();
+    const [tool, setTool] = useState<'rectangle' | 'polygon' | undefined>();
 
-  const anno = useAnnotator<AnnotoriousOpenSeadragonAnnotator>();
+    const anno = useAnnotator<AnnotoriousOpenSeadragonAnnotator>();
 
-  useEffect(() => {
-    if (!anno) return;
+    useEffect(() => {
+      if (!anno) return;
 
-    anno.addAnnotation(ANNOTATION);
+      anno.addAnnotation(ANNOTATION);
 
-    return () => {
-      anno.clearAnnotations();
-    }
-  }, [anno]);
+      return () => {
+        anno.clearAnnotations();
+      }
+    }, [anno]);
 
-  return (
-    <div className="openseadragon-example">
-      <OpenSeadragonAnnotator
-        drawingEnabled={tool !== undefined}
-        tool={tool || 'rectangle'}>
-        <OpenSeadragonViewer
-          className="osd-container"
-          options={options} />
-      </OpenSeadragonAnnotator>
+    return (
+      <div className="openseadragon-example">
+        <OpenSeadragonAnnotator
+          drawingEnabled={tool !== undefined}
+          tool={tool || 'rectangle'}>
+          <OpenSeadragonViewer
+            className="osd-container"
+            options={options} />
+        </OpenSeadragonAnnotator>
 
-      <div className='actions'>
-        <button 
-          className={tool === undefined ? 'active' : undefined}
-          onClick={() => setTool(undefined)}>
-          <Move size={15} /> Move Image
-        </button>
+        <div className='actions'>
+          <button 
+            className={tool === undefined ? 'active' : undefined}
+            onClick={() => setTool(undefined)}>
+            <Move size={15} /> Move Image
+          </button>
 
-        <button
-          className={tool === 'rectangle' ? 'rectangle active' : 'rectangle'}
-          onClick={() => setTool('rectangle')}>
-          <Square size={15} /> Rectangle
-        </button>
+          <button
+            className={tool === 'rectangle' ? 'rectangle active' : 'rectangle'}
+            onClick={() => setTool('rectangle')}>
+            <Square size={15} /> Rectangle
+          </button>
 
-        <button
-          className={tool === 'polygon' ? 'polygon active' : 'polygon'}
-          onClick={() => setTool('polygon')}>
-          <TriangleRight size={15} /> Polygon
-        </button>
+          <button
+            className={tool === 'polygon' ? 'polygon active' : 'polygon'}
+            onClick={() => setTool('polygon')}>
+            <TriangleRight size={15} /> Polygon
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    )
+
+  }
+
+  OpenSeadragonExample = () => {
+    return (
+      <Annotorious>
+        <App />
+      </Annotorious>
+    )
+  };
 
 }
 
-export const OpenSeadragonExample = () => {
 
-  return (
-    <Annotorious>
-      <App />
-    </Annotorious>
-  )
-
-}
+export { OpenSeadragonExample };
---
+++ src/components/UsingAnnotoriousTable.tsx
@@ -0,0 +1,70 @@
+import { Check } from 'lucide-react';
+
+import './UsingAnnotoriousTable.css';
+
+export const UsingAnnotoriousTable = () => {
+
+  return (
+    <table className="using-annotorious">
+      <thead>
+        <tr>
+          <th></th>
+          <th>
+            <div>Images</div>
+          </th>
+          <th>
+            <div>OpenSeadragon</div>
+          </th>
+          <th>
+            <div>TypeScript Support</div>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <span>Vanilla JS</span>
+          </td>
+          <td>
+            <div><Check size={18} /></div>
+          </td>
+          <td>
+            <div><Check size={18} /></div>
+          </td>
+          <td>
+            <div><Check size={18} /></div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <span>React</span>
+          </td>
+          <td>
+            <div><Check size={18} /></div>
+          </td>
+          <td>
+            <div><Check size={18} /></div>
+          </td>
+          <td>
+            <div><Check size={18} /></div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <span>Svelte</span>
+          </td>
+          <td>
+            <div>incomplete</div>
+          </td>
+          <td>
+            <div><Check size={18} /></div>
+          </td>
+          <td>
+            <div><Check size={18} /></div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
+
+}
