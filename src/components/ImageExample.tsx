327bbf3dded4abdbed4312f6d65e8ceb932d8dde
Rainer Simon <rainer@rainersimon.io>
2024-08-24 11:01:05 +0200

    Minor fixes + about to solve the SSR/import problem

--- src/components/ImageExample.tsx
+++ src/components/ImageExample.tsx
@@ -1,83 +1,88 @@
 import { useEffect, useState } from 'react';
 import { TriangleRight, Square } from 'lucide-react';
-import { Annotorious, ImageAnnotator, ShapeType, useAnnotator } from '@annotorious/react';
 import type { AnnotoriousImageAnnotator, DrawingTool, ImageAnnotation, RectangleGeometry } from '@annotorious/react';
 
 import './ImageExample.css';
 import '@annotorious/react/annotorious-react.css';
 
-const SAMPLE: ImageAnnotation = {
-  id: '7fb76422-3a8c-4c87-bbad-7c8bb68399a0',
-  bodies: [],
-  target: {
-    annotation: '7fb76422-3a8c-4c87-bbad-7c8bb68399a0',
-    selector: {
-      type: ShapeType.RECTANGLE,
-      geometry: {
-        bounds: {
-            minX: 272,
-            minY: 169,
-            maxX: 393,
-            maxY: 259
-        },
-        x: 272,
-        y: 169,
-        w: 121,
-        h: 90,
-      } as RectangleGeometry
+let ImageExample: () => JSX.Element = () => null as unknown as JSX.Element; 
+
+if (typeof window !== 'undefined') {
+  const { Annotorious, ImageAnnotator, ShapeType, useAnnotator } = await import('@annotorious/react');
+
+  const SAMPLE: ImageAnnotation = {
+    id: '7fb76422-3a8c-4c87-bbad-7c8bb68399a0',
+    bodies: [],
+    target: {
+      annotation: '7fb76422-3a8c-4c87-bbad-7c8bb68399a0',
+      selector: {
+        type: ShapeType.RECTANGLE,
+        geometry: {
+          bounds: {
+              minX: 272,
+              minY: 169,
+              maxX: 393,
+              maxY: 259
+          },
+          x: 272,
+          y: 169,
+          w: 121,
+          h: 90,
+        } as RectangleGeometry
+      }
     }
-  }
-};
+  };
 
-const App = () => {
+  const App = () => {
+    const anno = useAnnotator<AnnotoriousImageAnnotator>();
 
-  const anno = useAnnotator<AnnotoriousImageAnnotator>();
+    const [tool, setTool] = useState<DrawingTool>('rectangle');
 
-  const [tool, setTool] = useState<DrawingTool>('rectangle');
+    useEffect(() => {
+      if (!anno) return;
 
-  useEffect(() => {
-    if (!anno) return;
+      anno.addAnnotation(SAMPLE);
 
-    anno.addAnnotation(SAMPLE);
+      anno.on('createAnnotation', a => console.log(a));
 
-    anno.on('createAnnotation', a => console.log(a));
+      return () => {
+        anno.clearAnnotations();
+      }
+    }, [anno]);
 
-    return () => {
-      anno.clearAnnotations();
-    }
-  }, [anno]);
+    return (
+      <div className="image-example">      
+        <ImageAnnotator 
+          tool={tool}>
+          <img src="/640px-Hallstatt.jpg" />
+        </ImageAnnotator>
 
-  return (
-    <div className="image-example">      
-      <ImageAnnotator 
-        tool={tool}>
-        <img src="/640px-Hallstatt.jpg" />
-      </ImageAnnotator>
+        <div className='actions'>
+          <button 
+            className={tool === 'rectangle' ? 'active' : undefined}
+            onClick={() => setTool('rectangle')}>
+            <Square size={15} /> Rectangle
+          </button>
 
-      <div className='actions'>
-        <button 
-          className={tool === 'rectangle' ? 'active' : undefined}
-          onClick={() => setTool('rectangle')}>
-          <Square size={15} /> Rectangle
-        </button>
-
-        <button
-          className={tool === 'polygon' ? 'polygon active' : 'polygon'}
-          onClick={() => setTool('polygon')}>
-          <TriangleRight size={15} /> Polygon
-        </button>
+          <button
+            className={tool === 'polygon' ? 'polygon active' : 'polygon'}
+            onClick={() => setTool('polygon')}>
+            <TriangleRight size={15} /> Polygon
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 
+  ImageExample = () => {
+
+    return (
+      <Annotorious>
+        <App />
+      </Annotorious>
+    )
+
+  }
 }
 
-export const ImageExample = () => {
-
-  return (
-    <Annotorious>
-      <App />
-    </Annotorious>
-  )
-
-}
+export { ImageExample };
