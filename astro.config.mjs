3d187326c8f47df65138481e761d0edbe7947ead
Rainer Simon <rainer@rainersimon.io>
2024-08-25 16:31:32 +0200

    Added Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
     title: 'Annotorious',
-    customCss: [
-      './src/index.css',
-    ],
+    customCss: ['./src/index.css'],
     expressiveCode: {
-      styleOverrides: { borderRadius: '0.25rem' },
+      styleOverrides: {
+        borderRadius: '0.25rem'
+      },
       themes: ['nord']
     },
     social: {
@@ -19,26 +21,36 @@ export default defineConfig({
       label: 'Start Here',
       items: [{
         label: 'Getting Started',
-        link: 'getting-started',
+        link: 'getting-started'
       }, {
         label: 'Installation',
         slug: 'installation'
       }, {
         label: 'Migrating from v2.7',
-        link: 'migration-guide',
+        link: 'migration-guide'
       }]
     }, {
       label: 'Guides',
-      autogenerate: { directory: 'guides' }
+      autogenerate: {
+        directory: 'guides'
+      }
     }, {
       label: 'API Reference',
-      autogenerate: { directory: 'api-reference' }
+      autogenerate: {
+        directory: 'api-reference'
+      }
     }, {
       label: 'React',
-      autogenerate: { directory: 'react' }
+      autogenerate: {
+        directory: 'react'
+      }
     }, {
       label: 'Svelte',
-      autogenerate: { directory: 'svelte' }
+      autogenerate: {
+        directory: 'svelte'
+      }
     }]
-  }), react()]
+  }), react()],
+  output: "server",
+  adapter: cloudflare()
 });
