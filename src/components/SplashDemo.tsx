36b632882572d3a80284a2734f651df2403e2f0c
Rainer Simon <rainer@rainersimon.io>
2024-08-28 16:14:56 +0200

    Splash page + dark theme

--- src/components/SplashDemo.tsx
+++ src/components/SplashDemo.tsx
@@ -1,16 +1,43 @@
-import { ImageAnnotator } from '@annotorious/react';
+// import Highlight from 'react-highlight';
+import { Annotorious, ImageAnnotator, useAnnotations } from '@annotorious/react';
 
 import './SplashDemo.css';
 import '@annotorious/react/annotorious-react.css';
+// import 'highlight.js/styles/atom-one-dark.css';
+
+const AnnotatableImage = () => {
+
+  const annotations = useAnnotations();
+
+  return (
+    <div className="splash-demo">
+      <div className="glow" />
+
+      <div className="mock-browser">
+        <div className="mock-browser-toolbar">
+          <div className="input">Annotorious Demo</div>
+        </div>
+
+        <ImageAnnotator>
+          <img src="/images/schoenbrunn.jpg" />    
+        </ImageAnnotator>
+      </div>
+
+      {/*
+      <Highlight>
+        {JSON.stringify(annotations[0], null, 2)}
+      </Highlight>
+      */}
+    </div> 
+  )
+
+}
 
 export const SplashDemo = () => {
 
   return (
-    <div className="splash-demo">
-      <ImageAnnotator>
-        <img src="/images/schoenbrunn.jpg" />
-      </ImageAnnotator>
-    </div>
+    <Annotorious>
+      <AnnotatableImage />
+    </Annotorious>
   )
-
 }
