f368a893ac6b4e38f94efafa7631a34f5432ff52
Rainer Simon <rainer@rainersimon.io>
2024-09-12 07:52:48 +0200

    Work in progress

--- src/components/SplashDemo.tsx
+++ src/components/SplashDemo.tsx
@@ -1,10 +1,34 @@
+import { Annotorious, OpenSeadragonAnnotator, OpenSeadragonViewer } from '@annotorious/react';
+
 import './SplashDemo.css';
+import '@annotorious/react/annotorious-react.css';
+
+const options: OpenSeadragon.Options = {
+  defaultZoomLevel: 4.0,
+  prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@latest/build/openseadragon/images/',
+  tileSources: 'https://www.davidrumsey.com/luna/servlet/iiif/RUMSEY~8~1~305921~90076306/info.json',
+  gestureSettingsMouse: {
+    clickToZoom: false,
+    dblClickToZoom: false
+  },
+  minZoomLevel: 0.8,
+  visibilityRatio: 0.2,
+  preserveImageSizeOnResize: true
+};
 
 export const SplashDemo = () => {
 
   return (
     <div className="splash-demo">
-      <img src="/images/schoenbrunn.jpg" />
+      <Annotorious>
+        <OpenSeadragonAnnotator
+          drawingEnabled={true}
+          theme="dark">
+          <OpenSeadragonViewer
+            className="osd-container"
+            options={options} />
+        </OpenSeadragonAnnotator>
+      </Annotorious>
     </div>
   );
 
