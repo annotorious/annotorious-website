d95e4e5d06b246779b35f373b0d663bad29605b7
Rainer Simon <rainer@rainersimon.io>
2024-10-27 14:43:39 +0100

    Fixed popup doc errors

--- src/components/SplashDemo.tsx
+++ src/components/SplashDemo.tsx
@@ -1,20 +1,38 @@
-import { Annotorious, OpenSeadragonAnnotator, OpenSeadragonViewer } from '@annotorious/react';
+import { useEffect } from 'react';
+import OpenSeadragon from 'openseadragon';
+import { Annotorious, OpenSeadragonAnnotator, OpenSeadragonViewer, useViewer } from '@annotorious/react';
 
 import './SplashDemo.css';
 import '@annotorious/react/annotorious-react.css';
 
 const options: OpenSeadragon.Options = {
-  defaultZoomLevel: 4.0,
   prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@latest/build/openseadragon/images/',
-  tileSources: 'https://www.davidrumsey.com/luna/servlet/iiif/RUMSEY~8~1~305921~90076306/info.json',
+  tileSources: 'https://www.davidrumsey.com/luna/servlet/iiif/RUMSEY~8~1~291234~90062950/info.json',
   gestureSettingsMouse: {
     clickToZoom: false,
     dblClickToZoom: false
   },
   minZoomLevel: 0.8,
-  visibilityRatio: 0.2,
+  visibilityRatio: 1,
   preserveImageSizeOnResize: true
 };
+
+const InitialPosition = () => {
+
+  const viewer = useViewer();
+
+  useEffect(() => {
+    if (!viewer) return;
+
+    viewer.addHandler("open", function(){
+      var bounds = new OpenSeadragon.Rect(0.48, 0.44, 0.1, 0.1);
+      viewer.viewport.fitBounds(bounds, true);
+    });
+  }, [viewer]);
+
+  return null;
+
+}
 
 export const SplashDemo = () => {
 
@@ -22,11 +40,14 @@ export const SplashDemo = () => {
     <div className="splash-demo">
       <Annotorious>
         <OpenSeadragonAnnotator
-          drawingEnabled={true}
+          drawingEnabled={false}
           theme="dark">
+
           <OpenSeadragonViewer
             className="osd-container"
             options={options} />
+
+          <InitialPosition />
         </OpenSeadragonAnnotator>
       </Annotorious>
     </div>
